aa6fa495ba2dbb69f216e85f6264ecc04eef053c
Yulong Tan <yulongtan67@gmail.com>
2017-07-20 21:41:11 -0700

    adds more knockoutjs code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
 	function AppViewModel() {
 		this.firstName = 'Yulong';
 		this.lastName = 'Tan';
+
+		this.email = ko.observable('yulong.tan@alaskaair.com');
+		this.company = ko.observable('Alaska Airlines');
+
+		// Observables would've been called like functions()
+		//	return this.email() or return this.company()
+		this.fullName = ko.computed(function() {
+			return this.firstName + " " + this.lastName;
+		}, this);
 	}
 
 	// Activates knockoutjs
 	ko.applyBindings(new AppViewModel());
+
+
 });
